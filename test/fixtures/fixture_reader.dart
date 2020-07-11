b66c16562207381d3b6b2a4239f596086aac4442
Amal Pavithran <amalpavithranmp@gmail.com>
2020-07-11 22:50:17 +0530

    This should work...hopefully

--- test/fixtures/fixture_reader.dart
+++ test/fixtures/fixture_reader.dart
@@ -2,8 +2,9 @@ import 'dart:io';
 
 String fixture(String name) {
   var dir = Directory.current.path;
-  if (dir.endsWith('\\test')) {
+  if (dir.endsWith('\\test') || dir.endsWith('/test')) {
     dir = dir.replaceAll('\\test', '');
+    dir = dir.replaceAll('/test', '');
   }
   return File('$dir/test/fixtures/$name').readAsStringSync();
 }
